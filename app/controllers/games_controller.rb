282dba0ecc9416ca2a0baede1d373775120f5b18
Jason <jakuffler@gmail.com>
2021-10-04 18:20:43 -0400

    Backend is up

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,18 @@ class GamesController < ApplicationController
     get '/games' do
         Game.all.to_json
     end
+
+    post '/games' do
+       Game.create(title: params[:title], genre: params[:genre], platform: params[:platform], game_played: false).to_json       
+    end
+
+    patch '/games/:id' do
+        game = Game.find(params[:id])
+        game.update(params).to_json
+    end
+
+    delete '/games/:id' do
+        Game.find(params[:id]).destroy    
+    end
   
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
     get '/users' do
         User.all.to_json
     end
+
+    patch '/users/:id' do
+        # binding.pry
+        user = User.find(params[:id])
+        user.update(params)
+        user.to_json
+    end
+    
+    post '/users' do
+        User.create(name: params[:name], password: params[:password]).to_json
+    end
   
   end
